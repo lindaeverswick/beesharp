6b79205cfbc762510c190eff3fb108fc2ea83bc3
Henry Black <blackhaj@gmail.com>
2020-06-23 11:38:35 -0500

    Overhaul controllers to be DRYer

---
+++ server/controllers/CRUDControllers.js
@@ -0,0 +1,46 @@
+const db = require('../models/inventoryModel');
+
+const readOne = (model) => async (req, res, next) => {
+  console.log("inside readOne")
+  next()
+}
+
+const readMany = (model) => async (req, res, next) => {
+    // Set validation checks
+    
+    try {
+      const { rows } = await db.query(`
+        SELECT * FROM ${model}`
+      );
+      console.log(rows)
+      res.locals.payload = rows;
+      return next();
+    } catch (error) {
+      return next(error);
+    }
+}
+
+const createOne = (model) => async (req, res, next) => {
+  console.log("inside createOne")
+  next()
+}
+
+const updateOne = (model) => async (req, res, next) => {
+  console.log("inside updateOne")
+  next()
+}
+
+const deleteOne = (model) => async (req, res, next) => {
+  console.log("inside deleteOne")
+  next()
+}
+
+module.exports = (model) => {
+  return {
+    readOne: readOne(model),
+    readMany: readMany(model),
+    createOne: createOne(model),
+    updateOne: updateOne(model),
+    deleteOne: deleteOne(model),
+  }
+};
--- server/controllers/instrumentController.js
+++ server/controllers/instrumentController.js
@@ -1,36 +1,3 @@
-// IMPORT MODEL
-const db = require('../models/inventoryModel');
-// IMPORT CRUD CONTROLLER
+const CRUDControllers = require('./CRUDControllers');
 
-const readOne = async (req, res, next) => {
-  console.log("inside readOne")
-  next()
-}
-
-const readMany = async (req, res, next) => {
-  console.log("inside readMany")
-  next()
-}
-
-const createOne = async (req, res, next) => {
-  console.log("inside createOne")
-  next()
-}
-
-const updateOne = async (req, res, next) => {
-  console.log("inside updateOne")
-  next()
-}
-
-const deleteOne = async (req, res, next) => {
-  console.log("inside deleteOne")
-  next()
-}
-
-module.exports = {
-  readOne,
-  readMany,
-  createOne,
-  updateOne,
-  deleteOne,
-};
+module.exports = CRUDControllers('instruments');

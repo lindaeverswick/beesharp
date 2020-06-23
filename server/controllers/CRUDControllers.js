dbc06397765e76f550ed06f4ec3daf170a04c379
Henry Black <blackhaj@gmail.com>
2020-06-23 15:58:53 -0500

    Add full CRUD queries

--- server/controllers/CRUDControllers.js
+++ server/controllers/CRUDControllers.js
@@ -1,8 +1,20 @@
 const db = require('../models/inventoryModel');
 
 const readOne = (model) => async (req, res, next) => {
-  console.log("inside readOne")
-  next()
+  // Set validation checks
+  try {
+    const { rows } = await db.query(`
+      SELECT * 
+      FROM ${model}
+      WHERE id = $1`,
+      [req.params.id]
+    );
+    console.log(rows)
+    res.locals.payload = rows;
+    return next();
+  } catch (error) {
+    return next(error);
+  }
 }
 
 const readMany = (model) => async (req, res, next) => {
@@ -20,18 +32,100 @@ const readMany = (model) => async (req, res, next) => {
 }
 
 const createOne = (model) => async (req, res, next) => {
-  console.log("inside createOne")
-  next()
+  try {
+    // grab the keys and values
+    let keys = Object.keys(req.body);
+    let values = keys.map((el) => req.body[el])
+
+    // create strings for sql query
+    let valuesParameters = ""
+    let keysString = ""
+    for (let i = 0; i < values.length; i++){
+      if (i !== values.length -1){
+        keysString += `${keys[i]}, `
+        valuesParameters += `$${i+1}, `
+      } else {
+        keysString += `${keys[i]}`
+        valuesParameters += `$${i+1}`
+      }
+    }
+
+    // Make query
+    const { rows } = await db.query(`
+      INSERT INTO ${model} (${keysString})
+      VALUES (${valuesParameters}) 
+      RETURNING *`,
+      values
+    );
+    console.log(rows[0])
+    res.locals.payload = rows[0];
+    return next();
+  } catch (error) {
+    return next(error);
+  }
 }
 
 const updateOne = (model) => async (req, res, next) => {
-  console.log("inside updateOne")
-  next()
+  try {
+    
+    let keyValueString = "";
+    let keys = Object.keys(req.body);
+    let values = []
+    let id = req.params.id;
+    for (let i = 0; i < keys.length; i++ ){
+      if (keys[i] !== 'id') {
+        if (i !== keys.length -1){
+          keyValueString += `${keys[i]} = $${i+1}, `
+        } else {
+          keyValueString += `${keys[i]} = $${i+1}`
+        }
+        values.push(req.body[keys[i]])
+      }
+    }
+    values.push(id);
+
+    // Make query
+    const { rows } = await db.query(`
+      UPDATE
+        ${model}
+      SET
+        ${keyValueString}
+      WHERE
+        id = $${values.length}
+      RETURNING 
+        *`,
+      values
+    );
+    
+    console.log(rows[0])
+    res.locals.payload = rows[0];
+    return next();
+  } catch (error) {
+    return next(error);
+  }
 }
 
 const deleteOne = (model) => async (req, res, next) => {
-  console.log("inside deleteOne")
-  next()
+  try {
+    
+    let id = req.params.id;
+
+    // Make query
+    const { rows } = await db.query(`
+      DELETE FROM
+        ${model}
+      WHERE
+        id = $1
+      RETURNING 
+        *`,
+      [id]
+    );
+    console.log(rows[0])
+    res.locals.payload = rows[0];
+    return next();
+  } catch (error) {
+    return next(error);
+  }
 }
 
 module.exports = (model) => {

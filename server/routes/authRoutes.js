f4d116a31a1038e60f81a8ac63865161282c1066
Fan <fanny.shao18@gmail.com>
2020-06-24 11:55:49 -0400

    get session and user working

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,41 +3,58 @@ const passport = require('passport');
 const Strategy = require('passport-google-oauth20').Strategy;
 const path = require('path');
 const { clientID, clientSecret } = require('../secrets');
+const db = require('../models/inventoryModel');
+const { doesNotMatch } = require('assert');
+
 
 const router = express.Router();
 
 passport.use(new Strategy({
     clientID,
     clientSecret,
-    callbackURL: '/auth/auth/google/callback'
+    callbackURL: '/auth/google/callback'
   },
-  function(accessToken, refreshToken, profile, cb) {
+  async function(accessToken, refreshToken, profile, done) {
+    try {
+      let { rows } = await db.query(`
+      SELECT * 
+      FROM users
+      WHERE google_id = $1`, 
+      [profile.id])
 
-    // check if user is in the db (check based on Google ID)
-    // If yes, then pass user object into the done function
-    // If No user, then create the user and pass into the done function
-
-    console.log("accessToken", accessToken)
-    console.log("refreshToken", refreshToken)
-    console.log("profile", profile)
-    console.log("cb", cb)
-    return cb(null, profile);
+      let created = rows[0];
+      
+      if(!created){
+        let { rows } = await db.query(`INSERT INTO users (firstname, lastname, username, isadmin, google_id)
+                  VALUES ($1, $2, $3, $4, $5) RETURNING *;`, 
+                  [profile.name.givenName, profile.name.familyName, profile.name.givenName + profile.name.familyName, 'false', profile.id])
+        created = rows[0];   
+      }
+      done(null, created)
+    } catch (error) {
+      console.log(error.message)
+    }
   }));
 
 
+
+
+
 passport.serializeUser(function(user, cb) {
-  console.log("USER: ", user)
   cb(null, user.id);
 });
 
-passport.deserializeUser(function(id, cb) {
+passport.deserializeUser(async function(id, cb) {
+  console.log('check inside deserialize')
   // find user in DB, run callback with (err, userObject)
-  console.log("DESERIALIZE")
-  cb(null, id);
-});
+  let { rows } = await db.query(`
+      SELECT * 
+      FROM users
+      WHERE id = $1`, 
+      [id])
 
-router.use(passport.initialize());
-router.use(passport.session());
+  cb(null, rows[0]);
+});
 
 
 // Define routes.
@@ -45,13 +62,12 @@ router.get('/login',
   function(req, res){
     
     res.sendFile(path.resolve(__dirname, '../login.html'));
-    console.log('INSIDE')
   });
 
-router.get('/auth/google',
+router.get('/google',
   passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] }));
 
-router.get('/auth/google/callback', 
+router.get('/google/callback', 
   passport.authenticate('google', { failureRedirect: '/login' }),
   function(req, res) {
     res.redirect('/');
